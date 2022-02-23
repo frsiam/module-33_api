d731def3206e3fe12487f4366763693a22df2e08
frsiam <siam.frs@gmail.com>
2022-02-23 11:34:49 +0600

    add class

--- js/post.js
+++ js/post.js
@@ -7,6 +7,7 @@ function displayPosts(posts){
     const postContainer = document.getElementById('posts')
     for(const post of posts){
         const div = document.createElement('div')
+        div.classList.add('post')
         div.innerHTML = `
         <h3>${post.title}</h3>
         <p>${post.body}</p>
