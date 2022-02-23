e1c379d998e054aa1890bb023492e62e40f8cf61
frsiam <siam.frs@gmail.com>
2022-02-23 16:13:20 +0600

    create a div and p , appendchild and some style of p

--- js/album.js
+++ js/album.js
@@ -5,7 +5,16 @@ function loadAlbum(){
 }
 
 function displayAlbum(albums){
+    const div = document.getElementById('album')
     for(const album of albums){
-        
+        const p = document.createElement('p')
+        p.style.textAlign = 'center'
+        p.style.border = '2px solid green'
+        p.style.margin = '20px 100px'
+        p.style.padding = '20px 0'
+        p.style.backgroundColor = 'orange'
+        p.innerText = album.title
+        div.appendChild(p)
     }
 }
+loadAlbum()
